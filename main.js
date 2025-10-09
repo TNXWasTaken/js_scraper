94858be211c3f2bc4eeb026922992172744602c7
TNXWasTaken <118904290+TNXWasTaken@users.noreply.github.com>
2025-10-09 16:57:59 +0000

    changed it form axios ot fetch will remove the axios library soon

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ import axios from './node_modules/axios/dist/esm/axios.js';
 const website = 'https://www.example.com'
 
 // Using corsproxy.io - a reliable CORS proxy
-const res = await axios.get('https://corsproxy.io/?'.concat(website));
+const res = await fetch('https://corsproxy.io/?'.concat(website));
+const data = await res.text();
 console.log('Successfully fetched example.com!');
-console.log('HTML length:', res.data.length, 'characters');
-console.log('First 500 characters:', res.data.substring(0, 500));
+console.log('HTML length:', data.length, 'characters');
+console.log('First 500 characters:', data.substring(0, 500));
 
-var text = res.data;
+var text = data;
 var blob = new Blob([text], { type: 'text/plain' });
 var link = document.createElement('a');
 link.href = URL.createObjectURL(blob);
